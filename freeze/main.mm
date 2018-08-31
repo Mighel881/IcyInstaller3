29df9a823d7ce3f82050cffc9f311f7dc51df5ba
MidnightChip <34963874+midnightchip@users.noreply.github.com>
2018-08-31 16:07:46 -0600

    iOS 11 Support

--- freeze/main.mm
+++ freeze/main.mm
@@ -1,31 +1,38 @@
 #include <spawn.h>
 #include <signal.h>
+#include <dlfcn.h>
 
-void patch_setuid() {
-    void* handle = dlopen("/usr/lib/libjailbreak.dylib", RTLD_LAZY);
-    if (!handle) 
-        return;
-
-    // Reset errors
-    dlerror();
-    typedef void (*fix_setuid_prt_t)(pid_t pid);
-    fix_setuid_prt_t ptr = (fix_setuid_prt_t)dlsym(handle, "jb_oneshot_fix_setuid_now");
-    
-    const char *dlsym_error = dlerror();
-    if (dlsym_error) 
-        return;
-
-    ptr(getpid());
+/* Set platform binary flag */
+#define FLAG_PLATFORMIZE (1 << 1)
+void patch_setuidandplatformize() {
+        void* handle = dlopen("/usr/lib/libjailbreak.dylib", RTLD_LAZY);
+        if (!handle) return; // Reset errors
+        dlerror();
+        typedef void (*fix_setuid_prt_t)(pid_t pid);
+        fix_setuid_prt_t setuidptr = (fix_setuid_prt_t)dlsym(handle, "jb_oneshot_fix_setuid_now");
+        typedef void (*fix_entitle_prt_t)(pid_t pid, uint32_t what);
+        fix_entitle_prt_t entitleptr = (fix_entitle_prt_t)dlsym(handle, "jb_oneshot_entitle_now");
+        setuidptr(getpid()); setuid(0);
+        const char *dlsym_error = dlerror();
+        if (dlsym_error) {
+                return;
+        }
+        entitleptr(getpid(), FLAG_PLATFORMIZE);
 }
 
 int main(int argc, char **argv, char **envp) {
-	patch_setuid();
-	setuid(0);
-	setuid(0);
-	pid_t pid;
+        setuid(0);
+        if(getuid() != 0) {
+         patch_setuidandplatformize();
+         setuid(0);
+       }
+        setuid(0);
+        printf("uid: %d\n", getuid());
+        NSLog(@"CURRENTUID: %d", getuid());
+        pid_t pid;
         int status;
-	const char *path[] = {"PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/bin/X11:/usr/games", NULL};
+        const char *path[] = {"PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/bin/X11:/usr/games", NULL};
         posix_spawn(&pid, "/usr/bin/dpkg", NULL, NULL, (char**)argv, (char**)path);
         waitpid(pid, &status, 0);
-	return status;
+        return status;
 }
